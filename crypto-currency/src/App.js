8a0872f92d1a92e311852e71fa7b3299b15074c3
jeswanth <jachu.k1212@gmail.com>
2024-10-30 09:06:25 +0530

    Crypto_Currency

--- crypto-currency/src/App.js
+++ crypto-currency/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import HeroSection from './Components/HeroSection';
+import ServiceSection from './Components/ServiceSection';
 
 function App() {
   return (
     <div className="App">
       <HeroSection />
+      <ServiceSection />
     </div>
   );
 }
