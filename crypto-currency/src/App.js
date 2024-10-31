3a6161b34ce5b39bcc4a8a453c2422ebb581e9c9
jeswanth <jachu.k1212@gmail.com>
2024-10-31 08:33:37 +0530

    Crypto_Currency

--- crypto-currency/src/App.js
+++ crypto-currency/src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import HeroSection from './Components/HeroSection';
 import ServiceSection from './Components/ServiceSection';
+import HeaderSection from './Components/HeaderSection';
+import SliderSection from './Components/SliderSection';
 
 function App() {
   return (
     <div className="App">
       <HeroSection />
+      <HeaderSection />
+      <SliderSection />
       <ServiceSection />
     </div>
   );
