59cfb99aee5a06f6e8c3c7aaf92576dd040ca0c8
jeswanth <jachu.k1212@gmail.com>
2024-11-12 11:49:46 +0530

    Crypto_LIve

--- crypto-currency/src/Components/HeroSection.jsx
+++ crypto-currency/src/Components/HeroSection.jsx
@@ -1,21 +1,3 @@
-// import React, { Component } from 'react';
-// import heroBg from '../images/hero-bg.png';
-
-
-// class HeroBackground extends Component {
-//   render() {
-//     return (
-//       <div className="hero_bg_box">
-//         <div className="bg_img_box">
-//           <img src={heroBg} alt="" />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default HeroBackground;
-
 import React, { Component } from 'react';
 import heroBg from '../images/hero-bg.png';
 import sliderImg from '../images/slider-img.png';
